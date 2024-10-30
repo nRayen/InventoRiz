fe4925088447810d76ff27b8d1e3499b72049cad
nRayen <rayen.ndiaye@gmail.com>
2024-10-30 01:11:28 +0100

    (main) : added insert sql file

---
+++ insert.sql
@@ -0,0 +1,13 @@
+USE inventoriz;
+INSERT INTO Product (name, description, quantity, price)
+VALUES
+  ('Riz Basmati', 'Riz basmati de qualité supérieure, parfait pour les plats indiens', 20, 3.99),
+  ('Riz Jasmine', 'Riz jasmine parfumé, idéal pour les plats asiatiques', 15, 4.49),
+  ('Riz Arborio', 'Riz Arborio pour la préparation de risotto', 10, 5.99),
+  ('Riz Sushi', 'Riz spécialement conçu pour les sushis', 25, 6.49),
+  ('Riz Complet', 'Riz complet biologique, riche en fibres', 30, 3.49),
+  ('Riz Rouge', 'Riz rouge naturel, source de minéraux', 18, 7.99),
+  ('Riz Noir', 'Riz noir aux propriétés antioxydantes', 12, 8.49),
+  ('Riz Thaï', 'Riz thaï parfumé, populaire en cuisine asiatique', 22, 4.29),
+  ('Riz Gluant', 'Riz gluant utilisé dans les desserts asiatiques', 15, 5.19),
+  ('Riz Long Grain', 'Riz long grain, parfait pour les plats quotidiens', 40, 2.99);
